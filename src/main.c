7d516999fd006176020f56a2d51d4fcadfc2aa15
Alex Hoffman <alxhoff@gmail.com>
2020-11-23 10:06:41 +0100

    finished exercise 2

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+#include <espl_lib.h>
+
+int input;
+int more = 1;
+
+
 int main(){
 	printf("hello espl\n");
+	while(more == 1){
+		printf("enter an integer:\n");
+		scanf("%i", &input);
+		printf("%s", num_to_words(input));
+		printf("\ncontinue? (0/1)");
+		scanf("%i", &more);
+		}
 }
